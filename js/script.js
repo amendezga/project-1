24deb5c0a298beba5c2433b0106b433e087ba157
Alejandro <amendez1361@bths.edu>
2022-12-17 14:59:06 -0500

    wip: adding functions to retreive player data

--- js/script.js
+++ js/script.js
@@ -2,12 +2,26 @@ function getPlayerID() {
     return Math.floor(Math.random() * 493) + 1;
   }
 
-let playerID = getPlayerID();
+const userID = getPlayerID();
+
+let userPlayer;
+
+function getPlayerName() {
+    $.ajax({
+        type: 'GET',
+        url: 'https://www.balldontlie.io/api/v1/players/' + userID,
+        success: function(data) {
+        console.log(data)
+        userPlayer = `${data.first_name} ${data.last_name}`
+        console.log(userPlayer);
+        }
+    });
+}
 
 $(document).ready(function() {
     $('button').click(function() {
       $('.game-container').empty()
-      
-      getPlayerID();
+      getPlayerName()
+      $('.game-container').text(`Your player is ... ${userPlayer}`);
     });
   });
