1c3713a3b0f9df9d470c24d66a8c235e8f54008f
Alejandro <amendez1361@bths.edu>
2022-12-18 23:42:19 -0500

    added fn to add opponent to DOM

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function getPlayerID() {
-    return Math.floor(Math.random() * 493) + 1;
-  }
+  return Math.floor(Math.random() * 493) + 1;
+}
 
 const userID = getPlayerID();
 
@@ -9,27 +9,27 @@ let compPlayers = [];
 let compIDs = []
 
 function getUserName() {
-    $.ajax({
-        type: 'GET',
-        url: 'https://www.balldontlie.io/api/v1/players/' + userID,
-        success: function(data) {
-        userPlayer = `${data.first_name} ${data.last_name}`;
-        }
-    });
+  $.ajax({
+    type: 'GET',
+    url: 'https://www.balldontlie.io/api/v1/players/' + userID,
+    success: function (data) {
+      userPlayer = `${data.first_name} ${data.last_name}`;
+    }
+  });
 }
 
-function getOpponents() { 
-    for (let i = 0; i < 3; i++) {
-      let playerID = getPlayerID()
-      compIDs.push(playerID)
-      let comp = ''
-      let id = $.ajax({
-        type: 'GET',
-        url: `https://www.balldontlie.io/api/v1/players/${playerID}`,
-        success: function(data) {
+function getOpponents() {
+  for (let i = 0; i < 3; i++) {
+    let playerID = getPlayerID()
+    compIDs.push(playerID)
+    let comp = ''
+    let id = $.ajax({
+      type: 'GET',
+      url: `https://www.balldontlie.io/api/v1/players/${playerID}`,
+      success: function (data) {
         comp = `${data.first_name} ${data.last_name}`;
         compPlayers.push(comp)
-        }
+      }
     })
   }
 }
@@ -40,25 +40,29 @@ getOpponents();
 console.log(compPlayers)
 console.log(compIDs)
 
-$(document).ready(function() {
-    $('.start-game').click(function() {
-      $('.game-container').empty()
-      $('.game-container').text(`Your player is ... ${userPlayer}`);
-    });
+$(document).ready(function () {
+  $('.start-game').click(function () {
+    $('.game-container').html(
+      `Your player is ... ${userPlayer}
+      <br><br>
+      And your opponent is ... ${compPlayers[0]} 
+      <br<br><br><br><br><br><br><br><br><br>
+      <button class="next1">Next</button></div>`);
   });
+});
 
 
 
-  function simulateShots(threePct) {
-    const shotResults = [];
-  
-    for (let i = 0; i < 5; i++) {
-      const shotChance = Math.random();
-      if (shotChance < threePct) {
-       shotResults.push(`Shot ${i} made!`);
-      } else {
-        shotResults.push(`Shot ${i} missed!`);
-      }
+function simulateShots(threePct) {
+  const shotResults = [];
+
+  for (let i = 0; i < 5; i++) {
+    const shotChance = Math.random();
+    if (shotChance < threePct) {
+      shotResults.push(`Shot ${i} made!`);
+    } else {
+      shotResults.push(`Shot ${i} missed!`);
     }
-  
   }
+
+}
